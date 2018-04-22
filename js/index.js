b315582d9366cb22060c5ab1609e433030a8de84
Joana Mespaque <joanamespaque@gmail.com>
2018-04-22 19:53:56 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,1 @@
-const perfil = document.querySelector('.perfil');
-const menuHamburguer = document.querySelector('.navbar-toggle');
-const back = document.querySelector('.back-bt');
-const navbarMenu = document.querySelector('.navbar-collapse');
-menuHamburguer.addEventListener('click', function(){
-    navbarMenu.style.display = 'block';
-    perfil.style.display = 'block';
-    menuHamburguer.style.display = 'none';
-    back.style.display = 'block';
-});
-back.addEventListener('click', function(){
-    navbarMenu.style.display = 'none';
-    menuHamburguer.style.display = 'block';
-    back.style.display = 'none';
-    perfil.style.display = 'none';
-});
+var perfil=document.querySelector(".perfil"),menuHamburguer=document.querySelector(".navbar-toggle"),back=document.querySelector(".back-bt"),navbarMenu=document.querySelector(".navbar-collapse");menuHamburguer.addEventListener("click",function(){navbarMenu.style.display="block",perfil.style.display="block",menuHamburguer.style.display="none",back.style.display="block"}),back.addEventListener("click",function(){navbarMenu.style.display="none",menuHamburguer.style.display="block",back.style.display="none",perfil.style.display="none"});
