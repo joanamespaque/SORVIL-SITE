eb85b86f2683e6bb37c08b277ea6b588445c69cc
Joana Mespaque <joanamespaque@gmail.com>
2018-04-22 19:50:23 -0300

    Update editarperfil.js

--- js/editarperfil.js
+++ js/editarperfil.js
@@ -1,16 +1,1 @@
-const infoButton = document.querySelector('#infoButton');
-const contaButton = document.querySelector('#contaButton');
-const divInfo = document.querySelector('.inputsFormInfo');
-const divConta = document.querySelector('.inputsFormConta');
-contaButton.addEventListener('click', function(){
-    contaButton.style.backgroundColor = '#30afad';
-    infoButton.style.backgroundColor = 'white';
-    divConta.style.display = 'block';
-    divInfo.style.display = 'none'
-});
-infoButton.addEventListener('click', function(){
-    infoButton.style.backgroundColor = '#30afad';
-    contaButton.style.backgroundColor = 'white';
-    divInfo.style.display = 'block';
-    divConta.style.display = 'none';
-});
+var infoButton=document.querySelector("#infoButton"),contaButton=document.querySelector("#contaButton"),divInfo=document.querySelector(".inputsFormInfo"),divConta=document.querySelector(".inputsFormConta");contaButton.addEventListener("click",function(){contaButton.style.backgroundColor="#30afad",infoButton.style.backgroundColor="white",divConta.style.display="block",divInfo.style.display="none"}),infoButton.addEventListener("click",function(){infoButton.style.backgroundColor="#30afad",contaButton.style.backgroundColor="white",divInfo.style.display="block",divConta.style.display="none"});
